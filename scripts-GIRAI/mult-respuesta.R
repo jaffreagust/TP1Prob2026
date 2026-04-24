07298bc3cb72fd15403f6030239e33e5e429ada9
Ayrton Cuffaro <ayrtonciro05@gmail.com>
2026-04-24 00:06:27 -0300

    Multiple respuesta actualizado

--- scripts-GIRAI/mult-respuesta.R
+++ scripts-GIRAI/mult-respuesta.R
@@ -7,20 +7,34 @@ attach(datos)
 # Respuesta múltiple
 #p70_datpers, p70_suphum, p70_laboral, p70_segu, p70_transp
 
-tabla <- datos %>%
-  group_by(GIRAI_region) %>%
-  summarise(
-    across(c(p70_datpers, p70_suphum, p70_laboral, p70_segu, p70_transp),
-           ~sum(.x, na.rm = TRUE)),
-    ninguna = sum(ifelse(p70_datpers + p70_suphum + p70_laboral + p70_segu + p70_transp == 0, 1, 0)),
-    total_region = n()
-  ) %>%
-  pivot_longer(cols = c(p70_datpers, p70_suphum, p70_laboral, p70_segu, p70_transp, ninguna),
-               names_to = "variable",
-               values_to = "cant") %>%
-  group_by(GIRAI_region) %>%
-  mutate(
-    porcentaje = paste0(round(cant / total_region * 100, 2), "%")
-  ) %>%
-  arrange(GIRAI_region, desc(cant)) %>%
-  select(-total_region)  # Opcional: eliminar columna total_region
+# Calcular cantidad total de cada variable
+datos_cantidad <- datos %>% 
+  summarise(across(c(p70_datpers, p70_suphum, p70_laboral, p70_segu, p70_transp),
+                   ~sum(.x, na.rm = TRUE))) %>%
+  pivot_longer(cols = everything(), 
+               names_to = "variable", 
+               values_to = "cantidad") %>%
+  arrange(cantidad) %>%
+  mutate(variable = recode(variable,
+                           "p70_datpers" = "Protección de Datos y\n Privacidad",
+                           "p70_suphum" = "Supervición Humana",
+                           "p70_laboral" = "Protección Laboral y\n Derecho al Trabajo",
+                           "p70_segu" = "Seguridad, Precisión y\n Fiabilidad",
+                           "p70_transp" = "Transparencia y\n Explicabilidad"))
+  
+
+# Gráfico de barras
+datos_cantidad %>%
+  arrange(cantidad) %>%
+  mutate(variable = factor(variable, levels = variable)) %>%
+  ggplot(aes(x = cantidad, y = variable, fill = variable)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label = cantidad), 
+            hjust = -0.5, 
+            size = 4) +
+  labs(x = "Cantidad de paises", 
+       y = "Áreas temáticas", 
+       title = "Cantidad de paises con índices mayores a 70 puntos") +
+  theme_minimal() +
+  theme(axis.text.y = element_text(size = 11),
+        legend.position = "none")
